37574b432adcff41d1bbef05d5ae1e2b6b67380f
ifra abdi <ifraha461@gmail.com>
2025-06-06 23:14:06 +0300

    Complete lab: variable/String exercises

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+// Declare a constant currentUser
+const currentUser = 'Grace Hopper';
+
+// Create a welcome message using template literal
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Create an excited version of the welcome message using toUpperCase()
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create a short greeting that includes only the first initial of currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
